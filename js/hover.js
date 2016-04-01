71e1badbfbe53933fda272f12c0e870ded1ba1ac
cmuething <cmuething@mica.edu>
2016-03-31 21:10:56 -0400

    prototype 4 (?)

---
+++ js/hover.js
@@ -0,0 +1,220 @@
+$('.mat1').mouseenter(function(){
+        $('.mat1 .location').css('background-image', 'url("images/location_prop1.svg")');
+        $('.mat1 .media').css('background-image', 'url("images/media_prop1.svg")');
+        $('.mat1 .visual').css('background-image', 'url("images/visual_prop1.svg")');
+        $('.mat1 .content').css('background-image', 'url("images/content_prop1.svg")');
+        
+    }).mouseleave(function(){
+        $('.mat1 .location').css('background-image', 'url("images/location.svg")');
+        $('.mat1 .media').css('background-image', 'url("images/media.svg")');
+        $('.mat1 .visual').css('background-image', 'url("images/visual.svg")');
+        $('.mat1 .content').css('background-image', 'url("images/content.svg")');
+    });
+
+$('.mat2').mouseenter(function(){
+        $('.mat2 .location').css('background-image', 'url("images/location_prop1.svg")');
+        $('.mat2 .media').css('background-image', 'url("images/media_prop1.svg")');
+        $('.mat2 .visual').css('background-image', 'url("images/visual_prop1.svg")');
+        $('.mat2 .content').css('background-image', 'url("images/content_prop1.svg")');
+        
+    }).mouseleave(function(){
+        $('.mat2 .location').css('background-image', 'url("images/location.svg")');
+        $('.mat2 .media').css('background-image', 'url("images/media.svg")');
+        $('.mat2 .visual').css('background-image', 'url("images/visual.svg")');
+        $('.mat2 .content').css('background-image', 'url("images/content.svg")');
+    });
+
+$('.mat3').mouseenter(function(){
+        $('.mat3 .location').css('background-image', 'url("images/location_prop1.svg")');
+        $('.mat3 .media').css('background-image', 'url("images/media_prop1.svg")');
+        $('.mat3 .visual').css('background-image', 'url("images/visual_prop1.svg")');
+        $('.mat3 .content').css('background-image', 'url("images/content_prop1.svg")');
+        
+    }).mouseleave(function(){
+        $('.mat3 .location').css('background-image', 'url("images/location.svg")');
+        $('.mat3 .media').css('background-image', 'url("images/media.svg")');
+        $('.mat3 .visual').css('background-image', 'url("images/visual.svg")');
+        $('.mat3 .content').css('background-image', 'url("images/content.svg")');
+    });
+
+$('.mat4').mouseenter(function(){
+        $('.mat4 .location').css('background-image', 'url("images/location_prop1.svg")');
+        $('.mat4 .media').css('background-image', 'url("images/media_prop1.svg")');
+        $('.mat4 .visual').css('background-image', 'url("images/visual_prop1.svg")');
+        $('.mat4 .content').css('background-image', 'url("images/content_prop1.svg")');
+        
+    }).mouseleave(function(){
+        $('.mat4 .location').css('background-image', 'url("images/location.svg")');
+        $('.mat4 .media').css('background-image', 'url("images/media.svg")');
+        $('.mat4 .visual').css('background-image', 'url("images/visual.svg")');
+        $('.mat4 .content').css('background-image', 'url("images/content.svg")');
+    });
+
+$('.mat5').mouseenter(function(){
+        $('.mat5 .location').css('background-image', 'url("images/location_prop2.svg")');
+        $('.mat5 .media').css('background-image', 'url("images/media_prop2.svg")');
+        $('.mat5 .visual').css('background-image', 'url("images/visual_prop2.svg")');
+        $('.mat5 .content').css('background-image', 'url("images/content_prop2.svg")');
+        
+    }).mouseleave(function(){
+        $('.mat5 .location').css('background-image', 'url("images/location.svg")');
+        $('.mat5 .media').css('background-image', 'url("images/media.svg")');
+        $('.mat5 .visual').css('background-image', 'url("images/visual.svg")');
+        $('.mat5 .content').css('background-image', 'url("images/content.svg")');
+    });
+
+$('.mat6').mouseenter(function(){
+        $('.mat6 .location').css('background-image', 'url("images/location_prop2.svg")');
+        $('.mat6 .media').css('background-image', 'url("images/media_prop2.svg")');
+        $('.mat6 .visual').css('background-image', 'url("images/visual_prop2.svg")');
+        $('.mat6 .content').css('background-image', 'url("images/content_prop2.svg")');
+        
+    }).mouseleave(function(){
+        $('.mat6 .location').css('background-image', 'url("images/location.svg")');
+        $('.mat6 .media').css('background-image', 'url("images/media.svg")');
+        $('.mat6 .visual').css('background-image', 'url("images/visual.svg")');
+        $('.mat6 .content').css('background-image', 'url("images/content.svg")');
+    });
+
+$('.mat7').mouseenter(function(){
+        $('.mat7 .location').css('background-image', 'url("images/location_prop2.svg")');
+        $('.mat7 .media').css('background-image', 'url("images/media_prop2.svg")');
+        $('.mat7 .visual').css('background-image', 'url("images/visual_prop2.svg")');
+        $('.mat7 .content').css('background-image', 'url("images/content_prop2.svg")');
+        
+    }).mouseleave(function(){
+        $('.mat7 .location').css('background-image', 'url("images/location.svg")');
+        $('.mat7 .media').css('background-image', 'url("images/media.svg")');
+        $('.mat7 .visual').css('background-image', 'url("images/visual.svg")');
+        $('.mat7 .content').css('background-image', 'url("images/content.svg")');
+    });
+
+$('.mat8').mouseenter(function(){
+        $('.mat8 .location').css('background-image', 'url("images/location_prop2.svg")');
+        $('.mat8 .media').css('background-image', 'url("images/media_prop2.svg")');
+        $('.mat8 .visual').css('background-image', 'url("images/visual_prop2.svg")');
+        $('.mat8 .content').css('background-image', 'url("images/content_prop2.svg")');
+        
+    }).mouseleave(function(){
+        $('.mat8 .location').css('background-image', 'url("images/location.svg")');
+        $('.mat8 .media').css('background-image', 'url("images/media.svg")');
+        $('.mat8 .visual').css('background-image', 'url("images/visual.svg")');
+        $('.mat8 .content').css('background-image', 'url("images/content.svg")');
+    });
+
+$('.mat9').mouseenter(function(){
+        $('.mat9 .location').css('background-image', 'url("images/location_prop3.svg")');
+        $('.mat9 .media').css('background-image', 'url("images/media_prop3.svg")');
+        $('.mat9 .visual').css('background-image', 'url("images/visual_prop3.svg")');
+        $('.mat9 .content').css('background-image', 'url("images/content_prop3.svg")');
+        
+    }).mouseleave(function(){
+        $('.mat9 .location').css('background-image', 'url("images/location.svg")');
+        $('.mat9 .media').css('background-image', 'url("images/media.svg")');
+        $('.mat9 .visual').css('background-image', 'url("images/visual.svg")');
+        $('.mat9 .content').css('background-image', 'url("images/content.svg")');
+    });
+
+$('.mat10').mouseenter(function(){
+        $('.mat10 .location').css('background-image', 'url("images/location_prop3.svg")');
+        $('.mat10 .media').css('background-image', 'url("images/media_prop3.svg")');
+        $('.mat10 .visual').css('background-image', 'url("images/visual_prop3.svg")');
+        $('.mat10 .content').css('background-image', 'url("images/content_prop3.svg")');
+        
+    }).mouseleave(function(){
+        $('.mat10 .location').css('background-image', 'url("images/location.svg")');
+        $('.mat10 .media').css('background-image', 'url("images/media.svg")');
+        $('.mat10 .visual').css('background-image', 'url("images/visual.svg")');
+        $('.mat10 .content').css('background-image', 'url("images/content.svg")');
+    });
+
+$('.mat11').mouseenter(function(){
+        $('.mat11 .location').css('background-image', 'url("images/location_prop3.svg")');
+        $('.mat11 .media').css('background-image', 'url("images/media_prop3.svg")');
+        $('.mat11 .visual').css('background-image', 'url("images/visual_prop3.svg")');
+        $('.mat11 .content').css('background-image', 'url("images/content_prop3.svg")');
+        
+    }).mouseleave(function(){
+        $('.mat11 .location').css('background-image', 'url("images/location.svg")');
+        $('.mat11 .media').css('background-image', 'url("images/media.svg")');
+        $('.mat11 .visual').css('background-image', 'url("images/visual.svg")');
+        $('.mat11 .content').css('background-image', 'url("images/content.svg")');
+    });
+
+$('.mat12').mouseenter(function(){
+        $('.mat12 .location').css('background-image', 'url("images/location_prop3.svg")');
+        $('.mat12 .media').css('background-image', 'url("images/media_prop3.svg")');
+        $('.mat12 .visual').css('background-image', 'url("images/visual_prop3.svg")');
+        $('.mat12 .content').css('background-image', 'url("images/content_prop3.svg")');
+        
+    }).mouseleave(function(){
+        $('.mat12 .location').css('background-image', 'url("images/location.svg")');
+        $('.mat12 .media').css('background-image', 'url("images/media.svg")');
+        $('.mat12 .visual').css('background-image', 'url("images/visual.svg")');
+        $('.mat12 .content').css('background-image', 'url("images/content.svg")');
+    });
+
+$('.mat13').mouseenter(function(){
+        $('.mat13 .location').css('background-image', 'url("images/location_prop4.svg")');
+        $('.mat13 .media').css('background-image', 'url("images/media_prop4.svg")');
+        $('.mat13 .visual').css('background-image', 'url("images/visual_prop4.svg")');
+        $('.mat13 .content').css('background-image', 'url("images/content_prop4.svg")');
+        
+    }).mouseleave(function(){
+        $('.mat13 .location').css('background-image', 'url("images/location.svg")');
+        $('.mat13 .media').css('background-image', 'url("images/media.svg")');
+        $('.mat13 .visual').css('background-image', 'url("images/visual.svg")');
+        $('.mat13 .content').css('background-image', 'url("images/content.svg")');
+    });
+
+$('.mat14').mouseenter(function(){
+        $('.mat14 .location').css('background-image', 'url("images/location_prop4.svg")');
+        $('.mat14 .media').css('background-image', 'url("images/media_prop4.svg")');
+        $('.mat14 .visual').css('background-image', 'url("images/visual_prop4.svg")');
+        $('.mat14 .content').css('background-image', 'url("images/content_prop4.svg")');
+        
+    }).mouseleave(function(){
+        $('.mat14 .location').css('background-image', 'url("images/location.svg")');
+        $('.mat14 .media').css('background-image', 'url("images/media.svg")');
+        $('.mat14 .visual').css('background-image', 'url("images/visual.svg")');
+        $('.mat14 .content').css('background-image', 'url("images/content.svg")');
+    });
+
+$('.mat15').mouseenter(function(){
+        $('.mat15 .location').css('background-image', 'url("images/location_prop4.svg")');
+        $('.mat15 .media').css('background-image', 'url("images/media_prop4.svg")');
+        $('.mat15 .visual').css('background-image', 'url("images/visual_prop4.svg")');
+        $('.mat15 .content').css('background-image', 'url("images/content_prop4.svg")');
+        
+    }).mouseleave(function(){
+        $('.mat15 .location').css('background-image', 'url("images/location.svg")');
+        $('.mat15 .media').css('background-image', 'url("images/media.svg")');
+        $('.mat15 .visual').css('background-image', 'url("images/visual.svg")');
+        $('.mat15 .content').css('background-image', 'url("images/content.svg")');
+    });
+
+$('.mat16').mouseenter(function(){
+        $('.mat16 .location').css('background-image', 'url("images/location_prop4.svg")');
+        $('.mat16 .media').css('background-image', 'url("images/media_prop4.svg")');
+        $('.mat16 .visual').css('background-image', 'url("images/visual_prop4.svg")');
+        $('.mat16 .content').css('background-image', 'url("images/content_prop4.svg")');
+        
+    }).mouseleave(function(){
+        $('.mat16 .location').css('background-image', 'url("images/location.svg")');
+        $('.mat16 .media').css('background-image', 'url("images/media.svg")');
+        $('.mat16 .visual').css('background-image', 'url("images/visual.svg")');
+        $('.mat16 .content').css('background-image', 'url("images/content.svg")');
+    });
+
+$('.mat17').mouseenter(function(){
+        $('.mat17 .location').css('background-image', 'url("images/location_prop4.svg")');
+        $('.mat17 .media').css('background-image', 'url("images/media_prop4.svg")');
+        $('.mat17 .visual').css('background-image', 'url("images/visual_prop4.svg")');
+        $('.mat17 .content').css('background-image', 'url("images/content_prop4.svg")');
+        
+    }).mouseleave(function(){
+        $('.mat17 .location').css('background-image', 'url("images/location.svg")');
+        $('.mat17 .media').css('background-image', 'url("images/media.svg")');
+        $('.mat17 .visual').css('background-image', 'url("images/visual.svg")');
+        $('.mat17 .content').css('background-image', 'url("images/content.svg")');
+    });
